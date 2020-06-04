e5b436dd8ec9840b66a31e6f826cb4324444d0de
screenspan <19509110+screenspan@users.noreply.github.com>
2020-06-04 20:42:11 +0200

    Fetch files from cache; fetch offline page

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,7 @@ let CACHE_NAME = "testPWA"; // The string used to identify our cache
 const FILES_TO_CACHE = ["offline.html", "style.css"];
 
 self.addEventListener("install", (event) => {
-  console.log("installing...");
+  console.log("Installing...");
   event.waitUntil(
     caches
       .open(CACHE_NAME)
@@ -16,7 +16,7 @@ self.addEventListener("install", (event) => {
 });
 
 self.addEventListener("activate", (event) => {
-  console.log("activating...");
+  console.log("Activating...");
   event.waitUntil(
     caches.keys().then((keyList) => {
       return Promise.all(
@@ -31,16 +31,40 @@ self.addEventListener("activate", (event) => {
   );
 });
 
+// self.addEventListener("fetch", (event) => {
+//   if (event.request.mode !== "navigate") {
+//     // Not a page navigation, bail.
+//     return;
+//   }
+//   event.respondWith(
+//     fetch(event.request).catch(() => {
+//       return caches.open(CACHE_NAME).then((cache) => {
+//         return cache.match("offline.html");
+//       });
+//     })
+//   );
+// });
+
 self.addEventListener("fetch", (event) => {
-  if (event.request.mode !== "navigate") {
-    // Not a page navigation, bail.
-    return;
-  }
+  console.log("Fetch event for ", event.request.url);
   event.respondWith(
-    fetch(event.request).catch(() => {
-      return caches.open(CACHE_NAME).then((cache) => {
-        return cache.match("offline.html");
-      });
-    })
+    caches
+      .match(event.request)
+      .then((response) => {
+        if (response) {
+          console.log("Found ", event.request.url, " in cache");
+          return response;
+        }
+        console.log("Network request for ", event.request.url);
+        return fetch(event.request);
+
+        // TODO 4 - Add fetched files to the cache
+      })
+      .catch((error) => {
+        // TODO 6 - Respond with custom offline page
+        return caches.open(CACHE_NAME).then((cache) => {
+          return cache.match("offline.html");
+        });
+      })
   );
 });
